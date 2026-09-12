078564488b684a4954b073559c99211598ab9342
Abdallah-KH04 <abdallahkhwaileh@gmail.com>
2026-09-12 22:25:43 +0300

    added a clsBloodBankAdmin and clsAdmin Classes

---
+++ src/Domain/Entities/clsAdmin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class clsAdmin: User
+    {
+        private short _Permissions { get; set; }
+
+        public clsAdmin(short Permissions)
+        {
+            _Permissions = Permissions;
+        }
+        public void ViewFutureNeeds()
+        {
+
+        }
+
+        public bool RequestBlood()
+        {
+            return false;
+        }
+
+        public void ViewRecord()
+        {
+
+        }
+
+        public void ViewStorage()
+        {
+
+        }
+
+        public void Login()
+        {
+
+        }
+
+        public void Logout()
+        {
+
+        }
+    }
+}
---
+++ src/Domain/Entities/clsBloodBankAdmin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class clsBloodBankAdmin:clsAdmin
+    {
+        public void AddTargetedDonation() { }
+        public void AddGeneralDonation() { }
+        public void SetAvailabilityNegative() { }
+        public void SetAvailabilityPosative() { }
+        public void SendBloodIndeedSignal() { }
+    }
+}
