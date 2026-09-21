739067b9b067e36651d3631c4d9fe2d782b1b22b
Abdallah-KH04 <abdallahkhwaileh@gmail.com>
2026-09-21 03:26:37 +0300

    added Donation, Notification classess

---
+++ src/Domain/Entities/Donation.cs
@@ -0,0 +1,17 @@
+﻿
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    internal class Donation : BaseEntity
+    {
+        public string DonorId { get; set; } = string.Empty;
+
+        //public Donor Donor;
+        public BloodBank BloodBank { get; set; } = string.Empty;
+        public BloodUnit BloodUnit { get; set; } = string.Empty;
+
+        public DateTime DontaionDate { get; set; }
+
+    }
+}
---
+++ src/Domain/Entities/Notification .cs
@@ -0,0 +1,25 @@
+﻿
+
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    internal class Notification:BaseEntity
+    {
+        public string NotificationID { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+        //NotificationType NotificationType { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public DateTime ReadAtUtc { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; }
+        public Guid? RecipientUserId { get; set; }
+        public Guid? RecipientHospitalId { get; set; }
+        public Guid? RecipientBloodBankId { get; set; }
+        public Guid? RelatedBloodRequestId { get; set; }
+    }
+}
